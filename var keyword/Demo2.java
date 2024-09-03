9649f066a777485b44115adf0ecedc49251d1afb
Hiten <hitendewangan100j@gmail.com>
2024-09-03 19:30:33 +0530

    learning var

---
+++ var keyword/Demo2.java
@@ -0,0 +1,14 @@
+// Java program to demonstrate that
+// var can be used to declare a local variable
+
+class Demo2 {
+	public static void main(String[] args)
+	{
+
+		// local variable
+		var x = 100;
+
+		// print x to the console
+		System.out.println(x);
+	}
+}
